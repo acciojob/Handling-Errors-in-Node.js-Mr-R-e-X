8dd55097d5d5ad185a80d905cb958659aeb20c11
Mr-R-e-X <souvikhazra151@gmail.com>
2024-07-09 14:58:22 +0530

    done

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const fs = require('fs');
-
+const fs = require("fs");
+const http = require("http");
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.log("File not found");
+    } else {
+      console.log(data);
+    }
+  });
 }
 
 // TODO: Call printFileContents with the command-line argument
+printFileContents("output.txt");
+const server = http.createServer((req, res) => {
+  res.end("Hello World!");
+});
+
+server.listen(3000, () => {
+  console.log("Hello World!");
+});
